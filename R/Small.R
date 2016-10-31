125931a68a43e03922dbe78e33c5fc6fe154c920
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2016-10-31 16:39:05 -0700

    Removing mee object from code, we will use Multi assay experiment

--- R/Small.R
+++ R/Small.R
@@ -1,214 +1,96 @@
-#---------------return sample type function-------------------- -------
-# Returns type: tumor, bloodNormal, tissueNormal
-#according to the sampletypes
-# Returns type: tumor, bloodNormal, tissueNormal
-# tcgaSampleType.
-#x A TCGA sample barcode.
-# Tissue type: Tumor, Normal, Control, TRB, cell_line, XP, XCL
-tcgaSampleType <- function(x){
-  # "Code","Definition","Short Letter Code"
-  # "01","Primary solid Tumor","TP"
-  # "02","Recurrent Solid Tumor","TR"
-  # "03","Primary Blood Derived Cancer - Peripheral Blood","TB"
-  # "04","Recurrent Blood Derived Cancer - Bone Marrow","TRBM"
-  # "05","Additional - New Primary","TAP"
-  # "06","Metastatic","TM"
-  # "07","Additional Metastatic","TAM"
-  # "08","Human Tumor Original Cells","THOC"
-  # "09","Primary Blood Derived Cancer - Bone Marrow","TBM"
-  # "10","Blood Derived Normal","NB"
-  # "11","Solid Tissue Normal","NT"
-  # "12","Buccal Cell Normal","NBC"
-  # "13","EBV Immortalized Normal","NEBV"
-  # "14","Bone Marrow Normal","NBM"
-  # "20","Control Analyte","CELLC"
-  # "40","Recurrent Blood Derived Cancer - Peripheral Blood","TRB"
-  # "50","Cell Lines","CELL"
-  # "60","Primary Xenograft Tissue","XP"
-  # "61","Cell Line Derived Xenograft Tissue","XCL"  
-  code<-substring(x,14,15)
-  if (code < 10){
-    out <- "Tumor"
-  }else if ((code == 10) || (code == 11) || (code == 13) || (code == 14)){
-    out <- "Normal"
-  }else if (code==20){
-    out <- "Control"
-  }else if (code==40){
-    out <- "TRB"
-  }else if (code==50){
-    out <- "cell_line"
-  }else if (code==60){
-    out <- "XP"
-  }else if (code==61){
-    out <- "XCL"
-  }else{
-    out <- "other"
-  }
-  return(out)
-}
-
-# tcgaSampleType.
-# param tcgaId A vector list TCGA sample barcode.
-# return Standardized TCGA IDs
-standardizeTcgaId<-function(tcgaId){
-  out<-substring(tcgaId,1,15)
-  return(out)
-}
-
-
-
-
-
-
-
 
 ##############-----------fetch data
-## Construct MEE.data from load local data.
-#' fetch.mee
-#' @param meth A matrix or path of rda file only containing a matrix of 
-#' DNA methylation data.
-#' @param exp A matrix or path of rda file only containing a matrix of 
-#' expression data.
-#' @param sample A data frame or path of rda file only containing sample 
-#' information in data frame format.
-#' @param probeInfo A GRnage object or path of rda file only containing 
-#' a GRange of probe information 
-#' @param geneInfo A GRnage object or path of rda file only containing 
-#' a GRange of gene information (Coordinates, GENEID and SYMBOL) 
-#' @param probes A vector lists probes' name. If probes are specified, 
-#' the methylation and probeInfo will only contain this list of probes.
-#' @param genes A vector lists genes' ID. If gene are specified, 
-#' the methylation and probeInfo will only contain this list of probes.
+## Construct Multi Assay Experiment
+#' @description 
+#' This function will receive a gene expression and DNA methylation data objects 
+#' and create a Multi Assay Experiment.
+#' @param met A Summaerized Experiment, a matrix or path of rda file only containing the data.
+#' @param exp A Summaerized Experiment, a matrix or path of rda file only containing the data.
+#' @param pData A DataFrame or data.frame of the phenotype data for all participants
 #' @param TCGA A logical. FALSE indicate data is not from TCGA (FALSE is default). 
 #' TRUE indicates data is from TCGA and sample section will automatically filled in.
-#' @return A MEE.data object
+#' @return A MultiAssayExperiment object
 #' @export 
 #' @examples
-#' meth <- matrix(data=c(1:20),ncol=5,dimnames=list(paste0("probe",1:4),paste0("sample",1:5)))
-#' exp <- matrix(data=c(101:110),ncol=5,dimnames=list(c("gene1","gene2"),paste0("sample",1:5)))
-#' mee <- fetch.mee(meth=meth, exp=exp)
-#' ## only fetch probe 1 and 3
-#' mee <- fetch.mee(meth=meth, exp=exp, probes=c("probe1","probe3")) 
-#' ## only fetch gene 1
-#' mee <- fetch.mee(meth=meth, exp=exp, genes="gene1")
-fetch.mee <- function(meth,exp,sample,probeInfo,geneInfo,probes=NULL,
-                      genes=NULL,TCGA=FALSE){
-  if(!missing(meth)){
-    if(is.character(meth)){
-      newenv <- new.env()
-      load(meth, envir=newenv)
-      meth <- get(ls(newenv)[1],envir=newenv) # The data is in the one and only variable
-    }
-  }else{
-    meth <- NULL
-  }
+#' # NON TCGA example
+#' gene.exp <- DataFrame(sample1 = c("TP53"=2.3,"PTEN"=5.4),
+#'                        sample2 = c("TP53"=1.6,"PTEN"=2.3)
+#' )
+#' dna.met <- DataFrame(sample1 = c("cg22902499"=0.5,"cg06379435"=0.1),
+#'                        sample2 =  c("cg22902499"=0.3,"cg06379435"=0.9)
+#' )
+#' sample.info <- DataFrame(sample.type = c("Normal", "Tumor"))
+#' rownames(sample.info) <- colnames(gene.exp)
+#' mae <- create.mae(exp = gene.exp, met = dna.met, pData = sample.info) 
+#' \dontrun{
+#'    # TCGA example using TCGAbiolinks
+#'    library(TCGAbiolinks)
+#'    query <- GDCquery(project = "TCGA-ACC",
+#'                      data.category = "Gene expression",
+#'                      data.type = "Gene expression quantification",
+#'                      platform = "Illumina HiSeq", file.type  = "normalized_results",
+#'                      experimental.strategy = "RNA-Seq",
+#'                      barcode = c("TCGA-OR-A5J1","TCGA-OR-A5J2"),
+#'                      legacy = TRUE)
+#'    GDCdownload(query)
+#'    tcga.exp <- GDCprepare(query)
+#'    query <- GDCquery(project = "TCGA-ACC", 
+#'                      data.category = "DNA methylation", 
+#'                      platform = "Illumina Human Methylation 450",
+#'                      barcode = c("TCGA-OR-A5J1","TCGA-OR-A5J2"),
+#'                      legacy = TRUE)
+#'    GDCdownload(query)
+#'    tcga.met <- GDCprepare(query)
+#'    tcga.mae <- create.mae(exp = tcga.exp, met = tcga.met, TCGA = TRUE)
+#'    # To access the information
+#'    sampleMap(tcga.mae)
+#'    pData(tcga.mae)
+#'    mae.met <- experiments(tcga.mae)[["DNA methylation"]]
+#'    mae.exp <- experiments(tcga.mae)[["Gene expression"]]
+#' }
+#' create.mae
+create.mae <- function (exp, 
+                        met, 
+                        pData, 
+                        TCGA = FALSE) {
   
-  if(!missing(exp)){
-    if(is.character(exp)){
-      newenv <- new.env()
-      load(exp, envir=newenv)
-      exp <- get(ls(newenv)[1],envir=newenv) # The data is in the one and only variable
-    }
-  }else{
-    exp <- NULL
-  }
-  if(!missing(sample)){
-    if(is.character(sample)){
-      newenv <- new.env()
-      load(sample, envir=newenv)
-      sample <- get(ls(newenv)[1],envir=newenv) # The data is in the one and only variable
-      sample$ID <- as.character(sample$ID)
-    }
-  }else if(TCGA){
-    if(!is.null(meth) & is.null(exp)){
-      TN <- sapply(colnames(meth),tcgaSampleType)
-      TN[TN %in% "Tumor"] <- "Experiment"
-      TN[TN %in% "Normal"] <- "Control"
-      sample <- data.frame(ID=standardizeTcgaId(colnames(meth)),
-                           meth.ID=colnames(meth),
-                           TN=TN,
-                           stringsAsFactors = FALSE)
-    }else if(is.null(meth) & !is.null(exp)){
-      TN <- sapply(colnames(exp),tcgaSampleType)
-      TN[TN %in% "Tumor"] <- "Experiment"
-      TN[TN %in% "Normal"] <- "Control"
-      sample <- data.frame(ID=standardizeTcgaId(colnames(exp)),
-                           exp.ID=colnames(exp),
-                           TN=TN,
-                           stringsAsFactors = FALSE)
-    }else if(!is.null(meth) & !is.null(exp)){
-      ID <- intersect(standardizeTcgaId(colnames(meth)),standardizeTcgaId(colnames(exp)))
-      TN <- sapply(ID,tcgaSampleType)
-      TN[TN %in% "Tumor"] <- "Experiment"
-      TN[TN %in% "Normal"] <- "Control"
-      meth.ID <- colnames(meth)
-      exp.ID <- colnames(exp)
-      sample <- data.frame(ID=ID,meth.ID=meth.ID[match(ID,standardizeTcgaId(meth.ID))],
-                           exp.ID=exp.ID[match(ID,standardizeTcgaId(exp.ID))],
-                           TN=TN,stringsAsFactors = FALSE)
-      
-    }
-  }else if(!is.null(meth) & !is.null(exp)){
-    ID <- intersect(colnames(meth),colnames(exp))
-    if(length(ID)==0) 
-      stop("Sample naming should consistant in methylation and expression data.")
-    meth.ID <- colnames(meth)
-    exp.ID <- colnames(exp)
-    sample <- data.frame(ID=ID,meth.ID=meth.ID[match(ID,meth.ID)],
-                         exp.ID=exp.ID[match(ID,exp.ID)],stringsAsFactors = FALSE)
-  }else{
-    sample <- NULL
-  }
-  if(!missing(probeInfo)){
-    if(is.character(probeInfo)){
-      newenv <- new.env()
-      load(probeInfo, envir=newenv)
-      probeInfo <- get(ls(newenv)[1],envir=newenv) # The data is in the one and only variable
-    }
-    if(!is.null(probes)){
-      probeInfo <- probeInfo[probeInfo$name %in% probes]
-    }
-  }else{
-    probeInfo <- NULL
-  }
+  # Check if input are path to rda files
+  if(is.character(exp)) exp <- get(load(exp))
+  if(is.character(met)) exp <- get(load(met))
   
-  if(!missing(geneInfo)){
-    if(is.character(geneInfo)){
-      newenv <- new.env()
-      load(geneInfo, envir=newenv)
-      geneInfo <- get(ls(newenv)[1],envir=newenv) # The data is in the one and only variable
-    }
-    if(!is.null(genes)){
-      geneInfo <- geneInfo[geneInfo$GENEID %in% genes]
-    }
-  }else{
-    geneInfo <- NULL
-  }
-  
-  if(!is.null(probeInfo) & !is.null(meth)){
-    meth <- meth[rownames(meth) %in% as.character(probeInfo$name),]
-    probeInfo <- probeInfo[as.character(probeInfo$name) %in% rownames(meth)]
-  } 
-  if(!is.null(geneInfo) & !is.null(exp)){
-    exp <- exp[rownames(exp)%in% as.character(geneInfo$GENEID),]
-    geneInfo <- geneInfo[as.character(geneInfo$GENEID) %in% rownames(exp)]
-  } 
-  if(!is.null(meth) & !is.null(exp)){
-      if(TCGA){
-          meth <- meth[,sample$meth.ID]
-          exp <- exp[,sample$exp.ID]
-      }else{
-          meth <- meth[,sample$ID]
-          exp <- exp[,sample$ID]
-      }
+  if(TCGA){
+    message("Checking samples have both DNA methylation and Gene expression and they are in the same order...")
+    # If it is not TCGA we will assure the sample has both DNA methylation and gene expression
+    ID <- intersect(substr(colnames(met),1,15), substr(colnames(exp),1,15))
     
+    # Get only samples with both DNA methylation and Gene expression
+    met <- met[,match(ID,substr(colnames(met),1,15))]
+    exp <- exp[,match(ID,substr(colnames(exp),1,15))]
+    stopifnot(all(substr(colnames(exp),1,15) == substr(colnames(met),1,15)))
+    
+    # Get clinical information
+    if(missing(pData)) pData <- TCGAbiolinks:::colDataPrepare(c(colnames(met), colnames(exp)))
+    
+    message("Creating MultiAssayExperiment")
+    mae <- MultiAssayExperiment(experiments=list("DNA methylation" = met,
+                                                 "Gene expression" = exp),
+                                pData = pData,
+                                metadata = list(TCGA= TRUE))
+    
+  } else {
+    if(missing(pData)) stop("Please set pData argument. A data frame with samples information. All rownames should be colnames of DNA methylation and gene expression")
+    # Check that we have the same number of samples
+    ID <- intersect(colnames(met), colnames(exp))
+    met <- met[,match(ID,colnames(met))]
+    exp <- exp[,match(ID,colnames(exp))]
+    if(!all(colnames(exp) == colnames(met))) stop("Please, be sure your DNA methylation matrix and gene expression matrix have the samples in the same order")
+    mae <- MultiAssayExperiment(experiments=list("DNA methylation" = met,
+                                                 "Gene expression" = exp),
+                                pData = pData,
+                                metadata = list(TCGA=FALSE))
   }
-  mee <- mee.data(meth=meth,exp=exp,sample=sample,
-                  probeInfo=probeInfo,geneInfo=geneInfo)
-  invisible(gc())
-  return(mee)
+  return(mae)
 }
-
 
 #' fetch.pair
 #' @param pair A data.frame or path of csv file containing pair information.
@@ -226,7 +108,6 @@ fetch.mee <- function(meth,exp,sample,probeInfo,geneInfo,probes=NULL,
 #' geneInfo <- txs()
 #' ## input can be a path
 #' pair <- fetch.pair(pair = df, geneInfo=geneInfo)
-
 fetch.pair <- function(pair,probeInfo,geneInfo){
   if(!missing(pair)){
     if(is.character(pair)){
