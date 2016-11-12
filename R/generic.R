725f29e7b5f6d734107e432406da9695e7043283
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2016-11-11 17:07:57 -0800

    Man improvements

--- R/generic.R
+++ R/generic.R
@@ -1,4 +1,8 @@
-#' getPair
+#' getPair to extract pairInfo slot from Pair object.
+#' @description 
+#' getPair is a function to easily extract pairInfo out of a Pair object.
+#' By specifying geneID or probe, geneInfo for the defined genes (geneID ) and probes (probe)
+#' will be extracted out of Pair object. 
 #' @param object Pair object
 #' @param geneID A vector of genes' id. When specified, only the pair containing 
 #' these genes will be output.
