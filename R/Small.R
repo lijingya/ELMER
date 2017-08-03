554485c9867e298c00be12e7bd823243159e69a1
Tiago Chedraoui Silva <tiagochst@usp.br>
2017-08-03 18:52:14 +0000

    update
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ELMER@131733 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Small.R
+++ R/Small.R
@@ -613,43 +613,11 @@ get.GRCh <- function(genome = "hg38", genes) {
 #' @param classification Select if we will use Family classification or sub-family
 #' @return A list of TFs and its family members
 createMotifRelevantTfs <- function(classification = "family"){
-  message("Accessing hocomoco to get last version of TFs ", classification)
-  file <- paste0(classification,".motif.relevant.TFs.rda")
-  if(!file.exists(file)){
-    motif.relevant.TFs <- tryCatch({
-    # Download from http://hocomoco.autosome.ru/human/mono
-    tf.family <- "http://hocomoco.autosome.ru/human/mono" %>% read_html()  %>%  html_table()
-    tf.family <- tf.family[[1]]
-    # Split TF for each family, this will help us map for each motif which are the some ones in the family
-    # basicaly: for a TF get its family then get all TF in that family
-    col <- ifelse(classification == "family", "TF family","TF subfamily")
-    family <- split(tf.family,f = tf.family[[col]])
-    
-    motif.relevant.TFs <- plyr::alply(tf.family,1, function(x){  
-      f <- x[[col]]
-      if(f == "") return(x$`Transcription factor`) # Case without family, we will get only the same object
-      return(unique(family[as.character(f)][[1]]$`Transcription factor`))
-    },.progress = "text")
-    #names(motif.relevant.TFs) <- tf.family$`Transcription factor`
-    names(motif.relevant.TFs) <- tf.family$Model
-    # Cleaning object
-    attr(motif.relevant.TFs,which="split_type") <- NULL
-    attr(motif.relevant.TFs,which="split_labels") <- NULL
-    save(motif.relevant.TFs, file = file)
-    motif.relevant.TFs
-    }, error = function(e) {
-      message("Retrieving TFClass classification from ELMER.data.")
-      if(classification == "family") motif.relevant.TFs <- getdata("TF.family")
-      if(classification == "subfamily") motif.relevant.TFs <- getdata("TF.subfamily")
-      save(motif.relevant.TFs, file = file)
-      motif.relevant.TFs
-    }, finally = closeAllConnections())
-  } else {
-    motif.relevant.TFs <- get(load(file))
-  }
+  message("Retrieving TFClass classification from ELMER.data.")
+  if(classification == "family") motif.relevant.TFs <- getdata("TF.family")
+  if(classification == "subfamily") motif.relevant.TFs <- getdata("TF.subfamily")
   return(motif.relevant.TFs)
 }
-
 
 #' @title Filtering probes
 #' @description 
