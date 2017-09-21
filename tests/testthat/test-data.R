b3e817e519b2eac3b72dc59881395b6a644934d0
tiagochst <tiagochst@gmail.com>
2017-09-21 18:47:27 -0300

    fixing tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -4,12 +4,12 @@ test_that("Family of TF is correctly created from HOCOMOCO (TFClass database)", 
   # Create list of TF in the same family
   family <- createMotifRelevantTfs()
   # TP53 TP63 ad TP73 are in the same family
-  expect_true(all(c("TP53", "TP63", "TP73") %in% family$P53_HUMAN.H10MO.B))
-  expect_true(all(c("TP53", "TP63", "TP73") %in% family$P63_HUMAN.H10MO.A))
-  expect_true(all(c("TP53", "TP63", "TP73") %in% family$P73_HUMAN.H10MO.A))
-  expect_equal(length(family$P53_HUMAN.H10MO.B), 3)
-  expect_equal(length(family$P63_HUMAN.H10MO.A), 3)
-  expect_equal(length(family$P73_HUMAN.H10MO.A), 3)
+  expect_true(all(c("TP53", "TP63", "TP73") %in% family$P53_HUMAN.H11MO.0.A))
+  expect_true(all(c("TP53", "TP63", "TP73") %in% family$P63_HUMAN.H11MO.0.A))
+  expect_true(all(c("TP53", "TP63", "TP73") %in% family$P73_HUMAN.H11MO.0.A))
+  expect_equal(length(family$P53_HUMAN.H11MO.0.A), 3)
+  expect_equal(length(family$P63_HUMAN.H11MO.0.A), 3)
+  expect_equal(length(family$P73_HUMAN.H11MO.0.A), 3)
 })
 
 test_that("Get list of TF from uniprot database", {
