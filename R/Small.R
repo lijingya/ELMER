f8e1702f8b7e3053302a42f783b316b9182ce2e7
Tiago Chedraoui Silva <tiagochst@gmail.com>
2018-01-05 18:25:34 -0200

    bug fix

--- R/Small.R
+++ R/Small.R
@@ -374,7 +374,7 @@ makeSummarizedExperimentFromDNAMethylation <- function(met, genome, met.platform
   rowRanges <- annotation[rownames(met),,drop=FALSE]
   
   # Remove masked probes, besed on the annotation
-  rowRanges <- rowRanges[!rowRanges$MASK.general]
+  rowRanges <- rowRanges[!rowRanges$MASK_general]
   
   colData <-  DataFrame(samples = colnames(met))
   met <- met[rownames(met) %in% names(rowRanges),,drop = FALSE]
