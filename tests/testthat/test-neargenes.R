6c6c04c63a267e6826236514df9a3fa3e4cd1e65
Tiago Chedraoui Silva <tiagochst@gmail.com>
2019-03-19 09:41:06 -0300

    Improving testes

--- tests/testthat/test-neargenes.R
+++ tests/testthat/test-neargenes.R
@@ -7,6 +7,7 @@ test_that("It maps correctly to hg38", {
                    row.names =  c("cg18108049"),
                    names =  c("cg18108049"),
                    range = IRanges(start = c(16058489), end= c(16058489)))
+  # this are hg19 annotation 
   # chr1:16010827:16062808
   # chr1:16058489:16058489
   
@@ -30,6 +31,34 @@ test_that("It maps correctly to hg38", {
   expect_equal(NearbyGenes[NearbyGenes$Side == "L1",]$Symbol, "FAM131C")
   expect_equal(NearbyGenes[NearbyGenes$Side == "R1",]$GeneID, "ENSG00000142627")
   expect_equal(NearbyGenes[NearbyGenes$Side == "R1",]$Symbol, "EPHA2")
+})
+
+
+test_that("It maps correctly to hg38 if more than one region", {
+  tssAnnot <- getTSS(genome = "hg38")
+  geneAnnot <- TCGAbiolinks:::get.GRCh.bioMart(genome = "hg38",as.granges = TRUE)
+  probe <- getInfiniumAnnotation(genome = "hg19")[c("cg18108049","cg14008030","cg00381604","cg15254640","cg08417382")] 
+
+  NearbyGenes <- ELMER:::getRegionNearGenes(TRange = probe,
+                                            geneAnnot = geneAnnot,
+                                            numFlankingGenes = 4)
+  
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L2" & NearbyGenes$ID == "cg18108049",]$GeneID , "ENSG00000184908")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L1"  & NearbyGenes$ID == "cg18108049",]$GeneID, "ENSG00000185519")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L2" & NearbyGenes$ID == "cg18108049",]$Symbol, "CLCNKB")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L1" & NearbyGenes$ID == "cg18108049",]$Symbol, "FAM131C")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "R1" & NearbyGenes$ID == "cg18108049",]$GeneID, "ENSG00000142627")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "R1" & NearbyGenes$ID == "cg18108049",]$Symbol, "EPHA2")
+  
+  NearbyGenes <- GetNearGenes(numFlankingGenes = 20,
+                              geneAnnot = geneAnnot,
+                              TRange = probe)
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L2" & NearbyGenes$ID == "cg18108049",]$GeneID, "ENSG00000184908")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L1" & NearbyGenes$ID == "cg18108049",]$GeneID, "ENSG00000185519")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L2" & NearbyGenes$ID == "cg18108049",]$Symbol, "CLCNKB")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "L1" & NearbyGenes$ID == "cg18108049",]$Symbol, "FAM131C")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "R1" & NearbyGenes$ID == "cg18108049",]$GeneID, "ENSG00000142627")
+  expect_equal(NearbyGenes[NearbyGenes$Side == "R1" & NearbyGenes$ID == "cg18108049",]$Symbol, "EPHA2")
 })
 
 test_that("It maps correctly to hg19", {
