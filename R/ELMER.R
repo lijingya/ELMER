129fee72a7a3ca3221dc90265c0db07c1726078b
Tiago <tiagochst@gmail.com>
2020-04-14 11:49:32 -0400

    Reducing notes

--- R/ELMER.R
+++ R/ELMER.R
@@ -1,4 +1,5 @@
-#' ELMER is designed to use DNA methylation and gene expression from a
+#' @title ELMER (Enhancer Linking by Methylation/Expression Relationships)
+#' @description ELMER is designed to use DNA methylation and gene expression from a
 #' large number of samples to infere regulatory element landscape and transcription
 #' factor network in primary tissue.
 #' @docType package
